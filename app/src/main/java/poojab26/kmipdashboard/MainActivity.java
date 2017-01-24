3d1410f1d0a9712588405bf35ba514c8950882af
poojab26 <pbhaumik26@gmail.com>
2017-01-25 01:01:39 +0530

    Created java objects model for attributes

--- app/src/main/java/poojab26/kmipdashboard/MainActivity.java
+++ app/src/main/java/poojab26/kmipdashboard/MainActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import poojab26.kmipdashboard.Model.LogModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +21,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findMatch("34267585a");
+        stringToObjects();
     }
+    private void jsonSTuff(){
 
+        String json = "{\n" +
+                "    \"timestamp\": 1481626936000,\n" +
+                "    \"classname\": \"com.ibm.tklm.server.db.dao.jdbc.Utils\",\n" +
+                "    \"function_name\": \"createPreparedStatement(sql)\",\n" +
+                "    \"log_level\": \"FINER\",\n" +
+                "    \"log\": \"ENTRY INSERT INTO KMT_KMIP_ATTR_CRYPTOPARAMS(BLOCK_CIPHER_MODE,PADDING_METHOD,HASHING_ALGORITHM,ROLE_TYPE,MANAGED_OBJECT_UUID,INDEX_ID,RANDOM_IV,CRYPTOGRAPHIC_ALGORITHM) VALUES (?,?,?,?,?,?,?,?)\"\n" +
+                "  }";
+
+        try {
+
+            JSONObject obj = new JSONObject(json);
+
+            Log.d("My App", obj.toString());
+
+
+        } catch (Throwable t) {
+            Log.e("My App", "Could not parse malformed JSON: \"" + json + "\"");
+        }
+        finally {
+            Log.d("finally", "here");
+        }
+
+
+    }
     private void findMatch(String myString) {
 
         String match1 = "";
@@ -39,5 +71,19 @@ public class MainActivity extends AppCompatActivity {
 
         }
         Log.d("MATCH", match2);
+    }
+
+    public void stringToObjects(){
+        LogModel logModel = new LogModel();
+        logModel.setTimestamp(146000);
+        logModel.setClassname("com.ibm.tklm.server.db.dao.jdbc.Utils");
+        logModel.setFunctionName("createPreparedStatement(sql)");
+        logModel.setLogLevel("FINER");
+        logModel.setLog("ENTRY INSERT INTO KMT_KMIP_ATTR_CRYPTOPARAMS(BLOCK_CIPHER_MODE,PADDING_METHOD,HASHING_ALGORITHM,ROLE_TYPE,MANAGED_OBJECT_UUID,INDEX_ID,RANDOM_IV,CRYPTOGRAPHIC_ALGORITHM) VALUES (?,?,?,?,?,?,?,?)");
+
+
+        Gson gson = new Gson();
+
+        System.out.println(gson.toJson(logModel));
     }
 }
---
+++ app/src/main/java/poojab26/kmipdashboard/Model/LogModel.java
@@ -0,0 +1,87 @@
+package poojab26.kmipdashboard.Model;
+
+/**
+ * Created by pblead26 on 24-Jan-17.
+ */
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class LogModel {
+
+    @SerializedName("timestamp")
+    @Expose
+    private Integer timestamp;
+    @SerializedName("classname")
+    @Expose
+    private String classname;
+    @SerializedName("function_name")
+    @Expose
+    private String functionName;
+    @SerializedName("log_level")
+    @Expose
+    private String logLevel;
+    @SerializedName("log")
+    @Expose
+    private String log;
+
+    public LogModel(){};
+    public LogModel(Integer timestamp, String classname, String functionName, String logLevel, String log) {
+        super();
+        this.timestamp = timestamp;
+        this.classname = classname;
+        this.functionName = functionName;
+        this.logLevel = logLevel;
+        this.log = log;
+    }
+
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public String getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(String logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+}
+
