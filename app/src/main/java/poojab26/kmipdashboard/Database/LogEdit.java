17e1715f090ea0298a9eeed9dba63228f79fb0b2
poojab26 <pbhaumik26@gmail.com>
2017-03-27 16:45:23 +0530

    fix6:remove activity

--- app/src/main/java/poojab26/kmipdashboard/Database/LogEdit.java
+++ app/src/main/java/poojab26/kmipdashboard/Database/LogEdit.java
@@ -3,7 +3,7 @@ package poojab26.kmipdashboard.Database;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -13,7 +13,7 @@ import poojab26.kmipdashboard.R;
  * Created by pblead26 on 26-Mar-17.
  */
 
-public class LogEdit extends Activity implements View.OnClickListener {
+public class LogEdit extends Activity {
 
     private Button save;
     private String mode;
@@ -24,40 +24,21 @@ public class LogEdit extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_detail_page);
 
-        // get the values passed to the activity from the calling activity
-        // determine the mode - add, update or delete
-        if (this.getIntent().getExtras() != null){
-            Bundle bundle = this.getIntent().getExtras();
-            mode = bundle.getString("mode");
-        }
+        ContentValues values = new ContentValues();
+        values.put(LogsDb.KEY_TIMESTAMP, 333);
+        values.put(LogsDb.KEY_THREAD, "h");
+        values.put(LogsDb.KEY_CLASSNAME, "j");
+        values.put(LogsDb.KEY_FUNCTION, "k");
+        values.put(LogsDb.KEY_LOGLEVEL, "j");
+        values.put(LogsDb.KEY_LOGW, "k");
+        getContentResolver().insert(LoggerContentProvider.CONTENT_URI, values);
+        Log.d("Added", LogsDb.KEY_ROWID);
+        finish();
 
-        // get references to the buttons and attach listeners
-        save = (Button) findViewById(R.id.save);
-        save.setOnClickListener(this);
 
     }
 
-    public void onClick(View v) {
 
-
-        switch (v.getId()) {
-            case R.id.save:
-                ContentValues values = new ContentValues();
-                values.put(LogsDb.KEY_TIMESTAMP, 333);
-                values.put(LogsDb.KEY_THREAD, "h");
-                values.put(LogsDb.KEY_CLASSNAME, "j");
-                values.put(LogsDb.KEY_FUNCTION, "k");
-                values.put(LogsDb.KEY_LOGLEVEL, "j");
-                values.put(LogsDb.KEY_LOGW, "k");
-                getContentResolver().insert(LoggerContentProvider.CONTENT_URI, values);
-
-                finish();
-                break;
-
-
-
-        }
-    }
 
     // this sets the spinner selection based on the value
     private int getIndex(Spinner spinner, String myString){
