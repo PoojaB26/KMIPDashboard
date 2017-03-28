f198409c4642c67b44384e0dc20577540da0c8f3
poojab26 <pbhaumik26@gmail.com>
2017-03-29 01:08:54 +0530

    done: filter by date activity

--- app/src/main/java/poojab26/kmipdashboard/Filter/FilterActivity.java
+++ app/src/main/java/poojab26/kmipdashboard/Filter/FilterActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -17,36 +20,60 @@ import poojab26.kmipdashboard.R;
 
 public class FilterActivity extends Activity {
 
-    Button filterDate, filterLogLevel, filterResponse;
+    Button filterDate, filterLogLevel, filterButton, changeDatebtn;
+    CheckBox filterResponse;
     RadioGroup radioLoglevel;
     private RadioButton radioButtonLog;
+    LinearLayout dateView;
+
+
+    DatePicker picker;
+    String MY_PREFS_NAME = "DatePicker";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        picker=(DatePicker)findViewById(R.id.datePicker1);
 
         filterDate = (Button)findViewById(R.id.filterDate);
         filterLogLevel = (Button)findViewById(R.id.filterLogLevel);
-        filterResponse = (Button)findViewById(R.id.filterResponse);
+        filterResponse = (CheckBox)findViewById(R.id.filterResponse);
+        filterButton = (Button)findViewById(R.id.filterbtn);
+        changeDatebtn = (Button)findViewById(R.id.changeDatebtn);
+        dateView = (LinearLayout) findViewById(R.id.dateView);
         radioLoglevel=(RadioGroup)findViewById(R.id.radioGroupLog);
 
         filterDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent filterLogDate= new Intent(getBaseContext(),Activity_Date.class);
-                startActivity(filterLogDate);
+                dateView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        changeDatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent filterByDate = new Intent(getBaseContext(),FilterActivityDate.class);
+                filterByDate.putExtra("DATE", getCurrentDate());
+                startActivity(filterByDate);
             }
         });
 
         filterLogLevel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                radioLoglevel.setVisibility(View.VISIBLE);
+                      }
+        });
 
-                int selectedId=radioLoglevel.getCheckedRadioButtonId();
-                radioButtonLog=(RadioButton)findViewById(selectedId);
-                int index = radioLoglevel.indexOfChild(radioButtonLog);
-                Log.d("RADIO", Integer.toString(index));
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int index = getSelectedID();
+
 
                 Intent filterLogLevel= new Intent(getBaseContext(),FilterActivityLogLevel.class);
                 filterLogLevel.putExtra("LEVEL", Integer.toString(index));
@@ -59,5 +86,55 @@ public class FilterActivity extends Activity {
 
 
 
+    }
+
+    public String getCurrentDate(){
+        StringBuilder builder=new StringBuilder();
+        String month = "";
+        switch(picker.getMonth() + 1){
+            case 1: month = "Jan";
+                break;
+            case 2: month = "Feb";
+                break;
+            case 3: month = "Mar";
+                break;
+            case 4: month = "Apr";
+                break;
+            case 5: month = "May";
+                break;
+            case 6: month = "Jun";
+                break;
+            case 7: month = "Jul";
+                break;
+            case 8: month = "Aug";
+                break;
+            case 9: month = "Sep";
+                break;
+            case 10: month = "Oct";
+                break;
+            case 11: month = "Nov";
+                break;
+            case 12: month = "Dec";
+                break;
+        }
+//        builder.append("Current Date: ");
+        builder.append(month+" ");
+        builder.append(picker.getDayOfMonth()+", ");
+        builder.append(picker.getYear());
+
+      /*  SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("Date", builder.toString());
+        editor.commit();*/
+
+        return builder.toString();
+    }
+
+    private int getSelectedID() {
+        int selectedId=radioLoglevel.getCheckedRadioButtonId();
+        radioButtonLog=(RadioButton)findViewById(selectedId);
+        int index = radioLoglevel.indexOfChild(radioButtonLog);
+        Log.d("RADIO", Integer.toString(index));
+
+        return index;
     }
 }
